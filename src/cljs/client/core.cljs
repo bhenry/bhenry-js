ca5085dea5d87e1ac38fa2ee22046fd4fd287079
Brandon Henry <brandon.m.henry@gmail.com>
2013-11-06 01:13:03 -0500

    we have movement!!

--- src/cljs/client/core.cljs
+++ src/cljs/client/core.cljs
@@ -1,7 +1,13 @@
 (ns client.core
   (:require [dommy.core :as dommy]
-            [jayq.core :as j :refer [$]])
+            [jayq.core :as j :refer [$]]
+            [cljs.reader :refer [read-string]]
+            )
   (:use-macros [dommy.macros :only [deftemplate sel sel1]]))
+
+(def game-bug-count 30)
+(def game-board-width 20)
+(def game-board-height 10)
 
 (deftemplate layout [content]
   [:div#inner-content
@@ -17,30 +23,82 @@
                 :green)]
     [:i.fa.fa-bug.fa-flip-vertical {:style {:color color}}]))
 
+(deftemplate man [& [color]]
+  [:i.fa.fa-male {:style {:color (or color :black)}}])
+
+(deftemplate blank []
+  [:div.square])
+
 (deftemplate gameboard [h w]
   [:div#gameboard
-   [:table #_ {:border "1px" :border-collapse true}
+   [:table {:border "1px" :border-collapse true}
     (for [i (range h)]
       [:tr {:class (str i)}
        (for [j (range w)]
          [:td {:class (str j)
-               :data-coords (format "[%s,%s]" i j)}])])]])
+               :data-coords (format "[%s,%s]" j i)}
+          (blank)])])]])
 
-(defn change-state [[x y] val]
-  (let [$cell ($ (format "[data-coords='[%s,%s]']" x y))]
-    (j/html $cell val)))
+(defn grab [[x y]]
+  ($ (format "[data-coords='[%s,%s]']" x y)))
+
+(defn put [xy val]
+  (j/html (grab xy) val))
+
+(defn find-man []
+  (-> ($ ".fa-male") (j/closest "td")))
+
+(defn coords [$cell]
+  [(first (j/data $cell :coords))
+   (last (j/data $cell :coords))])
 
 (defn populate-board []
-  (doseq [i (range 5)]
-    (doseq [j (range 30)]
-      (if (= (rand-nth bug-states) :healthy)
-        (change-state [i j] (bug :healthy))))))
+  (put [0 5] (man))
+  (doseq [b (range game-bug-count)]))
+
+(defn validate-move [[x y] dir dist]
+  (condp = dir
+    :north (not (neg? (- y dist)))
+    :south (> game-board-height (+ y dist))
+    :west (not (neg? (- x dist)))
+    :east (> game-board-width (+ x dist))))
+
+(defn move [from to]
+  (let [dest (grab to)
+        curr (j/html (grab from))]
+    (put to curr)
+    (put from (blank))))
+
+(defn make-move [[x y] dir dist]
+  (when (validate-move [x y] dir dist)
+    (condp = dir
+      :north (move [x y] [x (- y dist)])
+      :south (move [x y] [x (+ y dist)])
+      :west (move [x y] [(- x dist) y])
+      :east (move [x y] [(+ x dist) y]))))
+
+(defn read-key-input [e]
+  (let [k (.-which e)]
+    (condp = k
+      38 :north
+      40 :south
+      37 :west
+      39 :east
+      :sit)))
+
+(defn wire-up-keyboard-controls []
+  (.keydown ($ "body")
+            (fn [e]
+              (let [dir (read-key-input e)]
+                (when-not (= dir :sit)
+                  (make-move (coords (find-man)) dir 1))))))
 
 (defn run []
-  (let [g (gameboard 20 30)
+  (let [g (gameboard game-board-height game-board-width)
         l (layout g)]
     (-> ($ "#content")
         (j/html l))
-    (populate-board)))
+    (populate-board)
+    (wire-up-keyboard-controls)))
 
 
